62fd34b17fc415ba28863eefa0d1b25706e7f640
DB400S2A-PC100\owner <sungman171@naver.com>
2016-12-16 09:36:32 +0900

    no message

--- src/common/constants/Session.java
+++ src/common/constants/Session.java
@@ -3,11 +3,7 @@ package common.constants;
 public interface Session {
 	
 	public static final String USER = "_USER_";
-<<<<<<< HEAD
-	public static final String ADMIN = "_ADMIN_";
-=======
 	public static final String ADMIN	= "_ADMIN_";
->>>>>>> 694f10d43037ea1d61ecbca51ad21b6db61b7f52
 	public static final String INSTRUCTOR = "_INSTRUCTOR_";
 
 }
