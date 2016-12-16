634831e883fd29c958680f7b0d516a0f4f04fd70
DB400S2A-PC109\owner <high1210@gmail.com>
2016-12-16 16:37:26 +0900

    정한이가 근재형한테 보내는 스키마

--- src/attendance/schema/AttendancesSchema.java
+++ src/attendance/schema/AttendancesSchema.java
@@ -18,6 +18,38 @@ public class AttendancesSchema {
 	private LecturesSchema lecturesSchema;
 	private String result;
 	private String cause;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(Date dateTime) {
+		this.dateTime = dateTime;
+	}
+	public UsersSchema getUsersSchema() {
+		return usersSchema;
+	}
+	public void setUsersSchema(UsersSchema usersSchema) {
+		this.usersSchema = usersSchema;
+	}
+	public LecturesSchema getLecturesSchema() {
+		return lecturesSchema;
+	}
+	public void setLecturesSchema(LecturesSchema lecturesSchema) {
+		this.lecturesSchema = lecturesSchema;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
 	public String getCause() {
 		return cause;
 	}
