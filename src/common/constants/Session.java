7fe19ad0d0f1efcbf3765f299164dc141423c327
이치훈 <clgns1202@gmail.com>
2016-12-19 15:18:54 +0900

    no message

--- src/common/constants/Session.java
+++ src/common/constants/Session.java
@@ -3,11 +3,7 @@ package common.constants;
 public interface Session {
 	
 	public static final String USER = "_USER_";
-<<<<<<< HEAD
-	public static final String ADMIN	= "_ADMIN_";
-=======
-	
->>>>>>> 06ab69b665ef9ed39cd7eb1689156e456985eb2a
+
 	public static final String INSTRUCTOR = "_INSTRUCTOR_";
 	
 	public static final String ADMIN = "_ADMIN_";
